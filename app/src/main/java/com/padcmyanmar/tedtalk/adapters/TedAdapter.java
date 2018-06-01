61944e6232473825be6fdf31f655dbd172d62ddb
NyiYeHan <nyiyehan@gmail.com>
2018-06-01 23:08:43 +0100

    UI implementation of TED Talks By Nyi Ye' Han.

---
+++ app/src/main/java/com/padcmyanmar/tedtalk/adapters/TedAdapter.java
@@ -0,0 +1,32 @@
+package com.padcmyanmar.tedtalk.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.padcmyanmar.tedtalk.R;
+import com.padcmyanmar.tedtalk.viewholders.ViewHolder;
+
+
+public class TedAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      LayoutInflater layoutInflater =  LayoutInflater.from(parent.getContext());//layout inflater obj
+       View view = layoutInflater.inflate(R.layout.activity_list,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 15;
+    }
+}
